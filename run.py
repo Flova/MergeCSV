884ff3e2407cf9afa84431057d6f19b82bb5c13b
Florian <florian@flova.de>
2018-02-24 00:03:02 +0100

    Updated Version with set implementation

--- run.py
+++ run.py
@@ -1,49 +1,56 @@
 import csv
 import pprint
+import operator
 import sys
+import datetime
 import tkinter as tk
+import json
 from tkinter import filedialog
 
 
-def read_csv(path,data,start):
+def read_csv(path):
     print("----- Start reading " + path + " -----")
-    raw = open(path, 'r', encoding = 'ISO-8859-1')
+    data = set()
+    raw = open(path, 'r', encoding = 'ISO 8859-1')
     content = csv.DictReader(raw, delimiter=';')
     headers = content.fieldnames
-    b_save = False
-    if(start == None):
-        b_save = True
     for row in content:
-        if (start == row and b_save == False):
-            b_save = True
-            continue
-        if (b_save == True):
-            #print(str(row) + "\n")
-            data.append(row)
+        data.add(json.dumps(row))
     print("----- Finished reading " + path + " -----\n")
     return (data, headers)
 
+def merge_csv(file_path):
+    data = set()
+    headers = []
+    for path in file_path:
+        (data_tmp, headers) = read_csv(path)
+        data = data.union(data_tmp)
+    raw_list = []
+    for data_set in data:
+        raw_list.append(json.loads(data_set))
+    return (raw_list, headers)
+
+def sort(list):
+    return sorted(list, key=lambda k: datetime.datetime.strptime(k['Buchungstag'], '%d.%m.%Y'))
 
 def run():
     root = tk.Tk()
-    # file_path = ["foo1.csv","foo.csv"]
+    #file_path = ["foo1.csv","foo.csv"]
     files = filedialog.askopenfilenames(parent=root,title='Choose a file')
     file_path = root.tk.splitlist(files)
-    last_line = None
-    data = []
-    headers = []
-    for path in file_path:
-        (data, headers) = read_csv(path, data, last_line)
-        last_line = data[-1]
+    # Main Function
+    (raw_list, headers) = merge_csv(file_path)
+    # Sort by Date
+    output_list = sort(raw_list)
     output_path = filedialog.asksaveasfilename(parent=root,title='Save the File')
+    #output_path = "csv1lol.csv"
     out_file  = open(output_path, "w")
     writer = csv.DictWriter(out_file, fieldnames=headers, delimiter=';')
     writer.writeheader()
-    for row in data:
+    for row in output_list:
         writer.writerow(row)
-
-    print(data)
-    print(len(data))
+    #pprint.pprint(output_list)
+    print("Datasets in final Dokument: " + str(len(output_list)))
 
 if __name__ == '__main__':
     run()
